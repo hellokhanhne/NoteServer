d5dc34e9647453e7297eb2f346ec6b2188c67080
Nguyen Ngoc Khanh <khanhjj70@gmail.com>
2021-09-09 14:02:44 +0700

    ok

---
+++ connectSql/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+async function connect() {
+   try {
+   await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gkzod.mongodb.net/noteManagement?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex : true,useFindAndModify:false})
+    console.log('success')
+   } catch (error) {
+       console.log('error')
+   }
+}
+
+module.exports = {connect}
